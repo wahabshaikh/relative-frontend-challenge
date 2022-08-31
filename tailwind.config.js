b4442f2e0cf8a9623392edf2e513a23568013f8f
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2022-08-31 23:37:26 +0530

    feat: set up tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Tomorrow", "sans-serif"],
+      },
+      colors: {
+        accent: "#ECF0FF",
+        primary: "#737BAE",
+        secondary: "#5A5F7D",
+        green: "#00FFA3",
+        red: "#FF4D4D",
+      },
+    },
   },
   plugins: [],
-}
+};
